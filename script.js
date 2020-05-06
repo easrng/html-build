aa584245dcd07705da76e4e68904f9916b347290
Glitch (hello-webpage) <none>
2020-05-06 14:51:16 +0000

    🦄❤️ Checkpoint ./script.js:3854026/6573 ./.glitch-assets:3854026/409

--- script.js
+++ script.js
@@ -2,18 +2,44 @@
 let compilers = {};
 let allNwjs = {
   windows: (async () => {
-    return await (await fetch()).blob();
-    //"https://yacdn.org/serve/https://dl.nwjs.io/v0.45.5/nwjs-v0.45.5-win-x64.zip"
+    return await (await fetch(
+      "https://yacdn.org/serve/https://dl.nwjs.io/v0.45.5/nwjs-v0.45.5-win-x64.zip"
+    )).blob();
   })(),
   mac: (async () => {
     return await (await fetch(
       "https://yacdn.org/serve/https://dl.nwjs.io/v0.45.5/nwjs-v0.45.5-osx-x64.zip"
+    )).blob();
+  })(),
+  linux: (async () => {
+    return await (await fetch(
+      "https://cdn.glitch.com/81b8b52c-881a-4697-bf55-08cc61865172%2Fnwjs-v0.45.5-linux-x64.zip?v=1588776347141"
     )).blob();
   })()
 };
 
 compilers.windows = async o => {
   let nwjs = await allNwjs.windows;
+  let zip = await JSZip.loadAsync(nwjs);
+  let prefix = Object.keys(zip.files)
+    .join("\n")
+    .match(/^(.+\/)credits.html$/m)[1];
+  zip.file(
+    prefix + "package.json",
+    JSON.stringify({
+      name: o.name,
+      main: "index.html",
+      icons: { "16": "icon.png" }
+    })
+  );
+  zip.file(prefix + "icon.png", o.icon);
+  zip.file(prefix + "index.html", o.html);
+  let zipBlob = await zip.generateAsync({ type: "blob" });
+  return zipBlob;
+};
+
+compilers.linux = async o => {
+  let nwjs = await allNwjs.linux;
   let zip = await JSZip.loadAsync(nwjs);
   let prefix = Object.keys(zip.files)
     .join("\n")
@@ -73,5 +99,5 @@ async () => {
   };
   o.icon = await (await fetch("https://yacdn.org/serve/" + o.icon)).blob();
 
-  location.href = URL.createObjectURL(await compilers.windows(o));
+  location.href = URL.createObjectURL(await compilers.linux(o));
 };
