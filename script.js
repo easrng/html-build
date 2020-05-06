d0f3e996929c79072d4d7e05ccb703bb55fa10a1
Glitch (hello-webpage) <none>
2020-05-06 14:15:17 +0000

    ⛲️🌋 Checkpoint ./script.js:3854026/6506

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-let compilers={}
-let nwjs={
-  windows:(()=>{})()
-}
+/* global JSZip */
+let compilers = {};
+let allNwjs = {
+  windows: (async () => {
+    return await (await fetch(
+      "https://yacdn.org/serve/https://dl.nwjs.io/v0.45.5/nwjs-v0.45.5-win-x64.zip"
+    )).blob();
+  })()
+};
 
-compilers.windows=async o=>{
-  
-let nwjs=await nwjs.win
-let zip=await JSZip.loadAsync(nwjs)
-let prefix=Object.keys(zip.files).join("\n").match(/^(.+\/)credits.html$/m)[1];
-o={name:"Test App", icon:"https://cdn2.scratch.mit.edu/get_image/user/default_90x90.png",html:`<!DOCTYPE html>
+compilers.windows = async o => {
+  let nwjs = await allNwjs.windows;
+  let zip = await JSZip.loadAsync(nwjs);
+  let prefix = Object.keys(zip.files)
+    .join("\n")
+    .match(/^(.+\/)credits.html$/m)[1];
+  zip.file(
+    prefix + "package.json",
+    JSON.stringify({
+      name: o.name,
+      main: "index.html",
+      icons: { "16": "icon.png" }
+    })
+  );
+  zip.file(prefix + "icon.png", o.icon);
+  zip.file(prefix + "index.html", o.html);
+  let zipBlob = await zip.generateAsync({ type: "blob" });
+  return zipBlob;
+};
+
+(async () => {
+  let o = {
+    name: "Test App",
+    icon: "https://cdn2.scratch.mit.edu/get_image/user/default_90x90.png",
+    html: `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8">
@@ -18,15 +42,9 @@ o={name:"Test App", icon:"https://cdn2.scratch.mit.edu/get_image/user/default_90
   <body>
   <h1>Hello, World!</h1>
   </body>
-</html>`}
-o.icon=await(await fetch("https://yacdn.org/serve/"+o.icon)).blob()
-zip.file(prefix+"package.json",JSON.stringify({
-    "name": o.name,
-    "main": "index.html",
-    "icons": { "16": "icon.png"}
-}))
-zip.file(prefix+"icon.png",o.icon)
-zip.file("index.html",o.html)
-let zipBlob=await zip.generateAsync({type:"blob"})
-location.href=URL.createObjectURL(zipBlob)
-}
+</html>`
+  };
+  o.icon = await (await fetch("https://yacdn.org/serve/" + o.icon)).blob();
+
+  location.href = URL.createObjectURL(await compilers.windows(o));
+})();
