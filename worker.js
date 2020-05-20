667f049f5a1c0a8c94d132885c025066ecb729a8
Glitch (hello-webpage) <none>
2020-05-20 15:55:38 +0000

    🚕🐌 Checkpoint ./text.html:3854026/20924 ./worker.js:3854026/323

--- worker.js
+++ worker.js
@@ -2,6 +2,14 @@
 importScripts("https://unpkg.com/comlink/dist/umd/comlink.js");
 importScripts("https://stuk.github.io/jszip/dist/jszip.js");
 
+function genUUID() {
+        // Reference: https://stackoverflow.com/a/2117523/709884
+        return ("10000000-1000-4000-8000-100000000000").replace(/[018]/g, s => {
+            const c = Number.parseInt(s, 10)
+            return (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        })
+}
+
 let compilers = {};
 let allNwjs = {
   windows: async () => {
--- wvapk.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-  let apk = await (await fetch(
-    "https://cdn.glitch.com/81b8b52c-881a-4697-bf55-08cc61865172%2FApp.apk?v=1588794922845"
-  )).blob();
-  let zip = await JSZip.loadAsync(apk);
-  window.zip = zip;
-})();
