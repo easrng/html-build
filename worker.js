a3c4af8d112eeeb40d1f1eecdb9f7ace6f2a3b75
Glitch (hello-webpage) <none>
2020-05-20 16:07:13 +0000

    💼📙 Checkpoint ./worker.js:3854026/11078 ./text.html:3854026/33 ./index.html:3854026/75

--- worker.js
+++ worker.js
@@ -3,11 +3,22 @@ importScripts("https://unpkg.com/comlink/dist/umd/comlink.js");
 importScripts("https://stuk.github.io/jszip/dist/jszip.js");
 
 function genUUID() {
-        // Reference: https://stackoverflow.com/a/2117523/709884
-        return ("10000000-1000-4000-8000-100000000000").replace(/[018]/g, s => {
-            const c = Number.parseInt(s, 10)
-            return (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-        })
+  // Reference: https://stackoverflow.com/a/2117523/709884
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, s => {
+    const c = Number.parseInt(s, 10);
+    return (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16);
+  });
+}
+
+function b2d(b) {
+  return new Promise(c => {
+    let f = new FileReader();
+    f.onload = e => c(f.result);
+    f.readAsDataURL(b);
+  });
 }
 
 let compilers = {};
@@ -51,10 +62,15 @@ compilers.windows = async o => {
   let prefix = Object.keys(zip.files)
     .join("\n")
     .match(/^(.+\/)credits.html$/m)[1];
-  await Promise.all(zip.folder(prefix).filter(e=>true).map(async e => {
-    zip.file("internal/" + e.name, await e.async("uint8array"));
-  }));
-  zip.remove(prefix)
+  await Promise.all(
+    zip
+      .folder(prefix)
+      .filter(e => true)
+      .map(async e => {
+        zip.file("internal/" + e.name, await e.async("uint8array"));
+      })
+  );
+  zip.remove(prefix);
   prefix = Object.keys(zip.files)
     .join("\n")
     .match(/^(.+\/)credits.html$/m)[1];
@@ -68,7 +84,7 @@ compilers.windows = async o => {
   );
   zip.file(prefix + "icon.png", o.icon);
   zip.file(prefix + "index.html", o.html);
-  zip.file("Launch.bat",`START "" ${prefix.replace(/\//g,"\\")}nw.exe`)
+  zip.file("Launch.bat", `START "" ${prefix.replace(/\//g, "\\")}nw.exe`);
   console.log("Generating app for Windows...");
   let zipBlob = await zip.generateAsync({ type: "blob" });
   console.log("Generated app for Windows!");
@@ -120,6 +136,61 @@ compilers.mac = async o => {
   let zipBlob = await zip.generateAsync({ type: "blob" });
   console.log("Generated app for Mac!");
   return zipBlob;
+};
+
+compilers.ios = async o => {
+  return new Blob([`<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>PayloadType</key>
+    <string>Configuration</string>
+    <key>PayloadVersion</key>
+    <integer>1</integer>
+    <key>PayloadOrganization</key>
+    <string>HTML Build</string>
+    <key>PayloadIdentifier</key>
+    <string>me.glitch.html-build.ios.app.${genUUID()}</string>
+    <key>PayloadUUID</key>
+    <string>${genUUID()}</string>
+    <key>PayloadDisplayName</key>
+    <string>${o.name}</string>
+    <key>PayloadDescription</key>
+    <string>App made with HTML Build.</string>
+    <key>PayloadRemovalDisallowed</key>
+    <false />
+    <key>PayloadContent</key>
+    <array>
+      <dict>
+        <key>PayloadType</key>
+        <string>com.apple.webClip.managed</string>
+        <key>PayloadVersion</key>
+        <integer>1</integer>
+        <key>PayloadIdentifier</key>
+        <string>me.glitch.html-build.ios.icon.${genUUID()}</string>
+        <key>PayloadUUID</key>
+        <string>${genUUID()}</string>
+        <key>PayloadDisplayName</key>
+        <string>App</string>
+        <key>PayloadDescription</key>
+        <string>App made with HTML Build.</string>
+        <key>PayloadOrganization</key>
+        <string>HTML Build</string>
+        <key>URL</key>
+        <string>${await b2d(o.html)}</string>
+        <key>Label</key>
+        <string>Test icon</string>
+        <key>Icon</key>
+        <data>iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=</data>
+        <key>IsRemovable</key>
+        <true />
+        <key>FullScreen</key>
+        <true />
+      </dict>
+    </array>
+  </dict>
+</plist>
+`],"application/x-apple-aspen-config");
 };
 
 Comlink.expose(compilers);
