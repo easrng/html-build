2948f52c9903fd11b9160dd32cddab90625d0730
easrng <easrng@gmail.com>
2022-02-02 18:33:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const log=document.querySelector("#log")
 compilers.setLogger(Comlink.proxy(text=>{
   log.value+=text+"\n"
 }));
+compilers.setStrings(Comlink.proxy(strings));
 /* global saveAs */
 document.querySelector("form").onsubmit = async e => {
   e.preventDefault();
