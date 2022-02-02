bf2c87fc60a8b35cf6e11161f8a14d6318f60d70
easrng <easrng@gmail.com>
2022-02-02 18:26:40 -0500

    Update strings.js

--- strings.js
+++ strings.js
@@ -2,7 +2,7 @@ const languages = ["en"]
 let language;
 
 if(languages.includes(navigator.language)) language = navigator.language
-else if (languages.includes(navigator.language.split("-")[0]) language = navigator.language.split("-")[0]
+else if(languages.includes(navigator.language.split("-")[0])) language = navigator.language.split("-")[0]
 else language = "en"
 
 const strings = {};
