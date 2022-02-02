2f5ca644d28a34a0a94b28828d9b84d9b663c754
easrng <easrng@gmail.com>
2022-02-02 18:30:46 -0500

    Update strings.js

--- strings.js
+++ strings.js
@@ -5,5 +5,4 @@ if(languages.includes(navigator.language)) language = navigator.language
 else if(languages.includes(navigator.language.split("-")[0])) language = navigator.language.split("-")[0]
 else language = "en"
 
-const strings = await import("./strings/"+language+".js");
-export {...strings}
+export default await import("./strings/"+language+".js");
